b837a9a0fbb93474e82f6ba2e7d6305c74770b20
Ankur Gupta <rukna1000@gmail.com>
2016-06-25 11:32:45 -0400

    Made the arduino be able to parse the incoming information coming from the Raspberry Pi

--- Experiments/Motor Power & Frames/Motor_Control/Motor_Control.ino
+++ Experiments/Motor Power & Frames/Motor_Control/Motor_Control.ino
@@ -1,4 +1,4 @@
-#inlcude <Stepper.h>
+#include <Stepper.h>
 
 const int PWM_A = 3;
 const int DIR_A = 12;
@@ -10,10 +10,58 @@ const int DIR_B = 13;
 const int BRAKE_B = 8;
 const int SNS_B = A1;
 
+void parseSerial(String input, int motorValues[]) {
+  char seperator = ':';
+  
+  int leftMotorPower = 0;
+  int rightMotorPower = 0;
+  
+  for(int i = 0; i < input.length(); i++) {
+    if(input.charAt(i) == seperator) {
+      leftMotorPower = input.substring(0, i).toInt();
+      rightMotorPower = input.substring(i+1, input.length() - 1).toInt();  
+    }
+  }
+  
+  motorValues[0] = leftMotorPower;
+  motorValues[1] = rightMotorPower;
+}
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  if(Serial.available()) {
+    parseSerial(Serial.read()); 
 
+    if(leftMotorPower > 0) {
+      digitalWrite(BRAKE_A, LOW);
+      digitalWrite(DIR_A, HIGH);    
+    }
+    else if(leftMotorPower < 0) {
+      digitalWrite(BRAKE_A, LOW);
+      digitalWrite(DIR_A, LOW);
+    }
+    else if(leftMototPower == 0) {
+      digitalWrite(BRAKE_A, HIGH);
+    }
+
+    if(rightMotorPower > 0) {
+      digitalWrite(BRAKE_B, LOW);
+      digitalWrite(DIR_B, HIGH);
+    }
+    else if(rightMotorPower < 0) {
+      digitalWrite(BRAKE_B, LOW);
+      digitalWrite(DIR_B, LOW);
+    }
+    else if(rightMotorPower == 0) {
+      digitalWrite(BRAKE_B, HIGH);
+    }
+
+    analogWrite(PWM_A, abs(leftMotorPower);
+    analogWrite(PWM_b, abs(rightMotorPower);  
+  }
+
+  delay(50);
 }
