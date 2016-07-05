baef3fe37b6fafbbc6c3e4f6c15c67796b0a9ec8
Ankur Gupta <rukna1000@gmail.com>
2016-07-04 22:05:56 -0400

    Added functions for weight and bias initalization

--- FeedForward/motorPower.py
+++ FeedForward/motorPower.py
@@ -2,5 +2,26 @@ import time
 import tensorflow as tf
 import numpy as np
 
+## Constants
+imageWidth = 640
+imageHeight = 480
+pixelCount = imageHeight * imageWidth
+
+auxCount = 4
+inputSize = auxCount + pixelCount
+
+## Functions
+
+
+def weight(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
 session = tf.InteractiveSession()
 
+input = tf.placeholder(tf.float32, shape=[None, inputSize])
