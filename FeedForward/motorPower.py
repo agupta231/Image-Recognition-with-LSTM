7e1eab1f7d38089f53e3cae790515aedb1fba8bd
Ankur Gupta <rukna1000@gmail.com>
2016-07-06 15:14:08 -0400

    Comitting changes for file transfer

--- FeedForward/motorPower.py
+++ FeedForward/motorPower.py
@@ -1,16 +1,14 @@
 import time
 import tensorflow as tf
 import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-## Constants
 imageWidth = 640
 imageHeight = 480
 pixelCount = imageHeight * imageWidth
 
 auxCount = 4
-inputSize = auxCount + pixelCount
-
-## Functions
 
 
 def weight(shape):
@@ -24,4 +22,7 @@ def bias(shape):
 
 session = tf.InteractiveSession()
 
-input = tf.placeholder(tf.float32, shape=[None, inputSize])
+W_conv1 = weight([15, 15, 1, 64])
+b_conv1 = bias([64])
+
+input_image = tf.reshape(indep, [-1, 28, 28, 1])
