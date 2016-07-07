bb5547f80ee36b2f636b607e453d094b76736e06
Ankur Gupta <rukna1000@gmail.com>
2016-07-07 12:27:19 -0400

    FInished image convolutions and pooling

--- FeedForward/motorPower.py
+++ FeedForward/motorPower.py
@@ -9,6 +9,7 @@ imageHeight = 480
 pixelCount = imageHeight * imageWidth
 
 auxCount = 4
+channels = 1
 
 
 def weight(shape):
@@ -20,9 +21,48 @@ def bias(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
+
+def conv2d(x, W, stride=1, padding=0):
+    x_padded = tf.pad(x, [[padding, padding], [padding, padding]], "CONSTANT")
+    return tf.nn.conv2d(x_padded, W, strides=[1, stride, stride, 1], padding='VALID')
+
+
+def max_pool(x, size, stride=2, padding=0):
+    x_padded = tf.pad(x, [[padding, padding], [padding, padding]], "CONSTANT")
+    return tf.nn.max_pool(x_padded, ksize=[1, size, size, 1], strides=[1, stride, stride, 1], padding='VALID')
+
+
 session = tf.InteractiveSession()
 
-W_conv1 = weight([15, 15, 1, 64])
-b_conv1 = bias([64])
-
+# Y'ALL GOTTA DEAL WITH THIS BAD LARRY LATER
 input_image = tf.reshape(indep, [-1, 28, 28, 1])
+
+
+W_conv1 = weight([4, 4, channels, 96])
+b_conv1 = bias([96])
+
+h_conv1 = tf.nn.relu(conv2d(input_image, W_conv1, stride=4) + b_conv1)
+h_pool1 = max_pool(h_conv1, 2)
+
+W_conv2 = weight([5, 5, 96, 128])
+b_conv2 = bias([128])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, padding=2) + b_conv2)
+h_pool2 = max_pool(h_conv2, 4)
+
+W_conv3 = weight([3, 3, 128, 256])
+b_conv3 = bias([256])
+
+h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3, padding=1) + b_conv3)
+
+W_conv4 = weight([3, 3, 256, 384])
+b_conv4 = bias([384])
+
+h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4, padding=1) + b_conv4)
+
+W_conv5 = weight([3, 3, 384, 256])
+b_conv5 = bias([256])
+
+h_conv5 = tf.nn.relu(conv2d(h_conv4, W_conv5, padding=1) + b_conv5)
+
+h_pool3 = max_pool(h_conv5, 4)
