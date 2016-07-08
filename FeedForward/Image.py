a0bdbc2a526471c9e2b8217cd254f770af651c18
Ankur Gupta <rukna1000@gmail.com>
2016-07-08 18:56:14 -0400

    Modified Image object to account for timestamp

--- FeedForward/Image.py
+++ FeedForward/Image.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
 class Image:
-    def __init__(self, imagePath, rightMotorPower, leftMotorPower, duration):
+    def __init__(self, imagePath, rightMotorPower, leftMotorPower, duration, ticks):
         self.path = imagePath
         self.rightMotorPower = tf.constant(rightMotorPower)
         self.leftMotorPower = tf.constant(leftMotorPower)
         self.duration = tf.constant(duration)
+        self.ticks = tf.constant(ticks)
