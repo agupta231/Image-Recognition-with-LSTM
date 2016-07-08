0fa3fbfc7b83a9f1ffd0a24d28447aaab7ddf2e6
Ankur Gupta <rukna1000@gmail.com>
2016-07-08 19:21:25 -0400

    Updated image object in order to make it look better when entering in data from the logFile

--- FeedForward/Image.py
+++ FeedForward/Image.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 class Image:
-    def __init__(self, imagePath, rightMotorPower, leftMotorPower, duration, ticks):
+    def __init__(self, imagePath, leftMotorPower, rightMotorPower, duration, ticks):
         self.path = imagePath
         self.rightMotorPower = tf.constant(rightMotorPower)
         self.leftMotorPower = tf.constant(leftMotorPower)
