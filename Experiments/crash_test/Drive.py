6edf1c00552a2e2679352637e24d253e87b3e230
Ankur Gupta <rukna1000@gmail.com>
2016-08-16 16:12:04 -0400

    Modifying Drive.py to be mutlithreaded as well as use ultrasonic data

--- Experiments/crash_test/Drive.py
+++ Experiments/crash_test/Drive.py
@@ -1,11 +1,19 @@
 import serial
+import threading
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-
 logFile = open("LogFile.txt", "a")
 
-while True:
-    power = raw_input("Motor Power:\n")
 
-    ser.write(power + ":" + power)
-    logFile.write(power + "\n")
+class new_thread(threading.Thread):
+    def __init__(self, process, baud_rate):
+        threading.Thread.__init__(self)
+        self.process = process
+        self.baud_rate = baud_rate
+
+        # self.process setup:
+        # 1 - Motor Management
+        # 2 - Ultra Sonic Data Management
+
+    def run(self):
+        pass
