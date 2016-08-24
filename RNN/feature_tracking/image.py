1f8d91f06f9cd2b2a091b5bcbb23beaa3baaf52e
Ankur Gupta <rukna1000@gmail.com>
2016-08-23 20:23:32 -0400

    Created method in image class to determine whether or not the current frame is a crash

--- RNN/feature_tracking/image.py
+++ RNN/feature_tracking/image.py
@@ -17,6 +17,17 @@ class Image:
     def to_tensor_with_aux_info(self):
         return tf.concat(0, [self._to_flattened_tensor(), np.array(self.left_motor_power), np.array(self.right_motor_power)]).eval()
 
+    def crash_one_hot(self):
+        # One hot vector format:
+        # 0 - Not a crash
+        # 1 - Is a crash
+        # Huh! not that bad...
+
+        if self.distance <= Image.threshold:
+            return np.array([0, 1])
+        else:
+            return np.array([1, 0])
+
     def _to_tensor(self):
         tensor = tf.image.decode_jpeg(tf.read_file(self.image_path), channels=Image.channels).eval()
         tensor_flattened = tf.reshape(tensor, [-1])
